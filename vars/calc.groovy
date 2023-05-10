ac5b9748651470e19818ce2592a61d361ddfec59
prasadcloud <127221508+prasadcloud@users.noreply.github.com>
2023-05-10 08:30:34 +0530

    Create caluculations

---
+++ vars/calc.groovy
@@ -0,0 +1,16 @@
+def sum(a,b)
+{
+  echo "The sum of ${a} and ${b} is ${a+b}"
+}
+def mul(a,b)
+{
+  echo "The multiplication of ${a} and ${b} is ${a*b}"
+}
+def subt(a,b)
+{
+  echo "The subtraction of ${a} and ${b} is ${a-b}"
+}
+def divi(a,b)
+{
+  echo "The division of ${a} and ${b} is ${a/b}"
+}
